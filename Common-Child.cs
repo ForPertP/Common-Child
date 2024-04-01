d0ffa4815e7b8fe1c947a79c51a60729cae8cc1e
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-04-02 07:19:31 +0900

    Update Common-Child.cs

--- Common-Child.cs
+++ Common-Child.cs
@@ -1,1 +1,30 @@
-C#
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'commonChild' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING s1
+     *  2. STRING s2
+     */
+
+    public static int commonChild(string s1, string s2)
+    {
+    }
+}
