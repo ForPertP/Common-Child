55789e5b3fd7cd00a64ddbee56afec532fe8ad5f
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-04-03 07:10:46 +0900

    Update Common-Child.cs

--- Common-Child.cs
+++ Common-Child.cs
@@ -28,3 +28,23 @@ class Result
     {
     }
 }
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s1 = Console.ReadLine();
+
+        string s2 = Console.ReadLine();
+
+        int result = Result.commonChild(s1, s2);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
